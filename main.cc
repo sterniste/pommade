94786652f2927d205783b08e1890f352b0ac2f4f
Stephen Stern <stephen.stern@gmail.com>
2016-05-31 09:34:52 +0200

    .

--- main.cc
+++ main.cc
@@ -1,17 +1,14 @@
 #include <iostream>
-#include <memory>
 
 #include <xercesc/sax/SAXParseException.hpp>
 #include <xercesc/util/XMLException.hpp>
 
 #include "rewrite_pom.h"
-#include "xml_graph.h"
 #include "xml_parser.h"
 
 namespace {
 using namespace std;
 using namespace pommade;
-using namespace xml_graph;
 using namespace xml_parser;
 using namespace xercesc_3_1;
 }
@@ -21,9 +18,7 @@ main(int argc, const char* argv[]) {
   try {
     basic_xml_doc_handler doc_handler;
     xml_doc_parser doc_parser{doc_handler};
-    const unique_ptr<const xml_node> doc{doc_parser.parse_doc(argv[1])};
-    const xml_node rw_pom{rewrite_pom(doc.get())};
-    cout << rw_pom;
+    cout << rewrite_pom(doc_parser.parse_doc(argv[1]).get());
   } catch (const XMLException& e) {
     cout << "caught XMLException: " << xmlstring{e.getMessage()} << endl;
     return 1;
